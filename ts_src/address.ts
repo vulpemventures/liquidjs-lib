af0a0a9eb72ffd8682e81f39359df4ef02c4a6f4
tiero <3596602+tiero@users.noreply.github.com>
2022-09-30 17:40:49 +0200

    add scriptPubKey field in fromConfidential response

--- ts_src/address.ts
+++ ts_src/address.ts
@@ -29,6 +29,7 @@ export interface Blech32Result {
 export interface ConfidentialResult {
   blindingKey: Buffer;
   unconfidentialAddress: string;
+  scriptPubKey?: Buffer;
 }
 
 const FUTURE_SEGWIT_MAX_SIZE: number = 40;
@@ -328,20 +329,15 @@ function fromConfidentialLegacy(
   if (payload.length < 55) throw new TypeError(address + ' is too short');
   if (payload.length > 55) throw new TypeError(address + ' is too long');
 
-  // Blinded decoded haddress has the form:
+  // Blinded decoded address has the form:
   // BLIND_PREFIX|ADDRESS_PREFIX|BLINDING_KEY|SCRIPT_HASH
   // Prefixes are 1 byte long, thus blinding key always starts at 3rd byte
   const blindingKey = payload.slice(2, 35);
-  const unconfidential = payload.slice(35, payload.length);
-  const versionBuf = Buffer.alloc(1);
-  versionBuf[0] = prefix;
-  const unconfidentialAddressBuffer = Buffer.concat([
-    versionBuf,
-    unconfidential,
-  ]);
-  const unconfidentialAddress = bs58check.encode(unconfidentialAddressBuffer);
-
-  return { blindingKey, unconfidentialAddress };
+  const script = payload.slice(35, payload.length);
+  const versionBuf = Buffer.of(prefix);
+  const scriptWithNetworkPrefix = Buffer.concat([versionBuf, script]);
+  const unconfidentialAddress = bs58check.encode(scriptWithNetworkPrefix);
+  return { blindingKey, unconfidentialAddress, scriptPubKey: script };
 }
 
 function fromConfidentialSegwit(
@@ -350,7 +346,11 @@ function fromConfidentialSegwit(
 ): ConfidentialResult {
   const result = fromBlech32(address);
   const unconfidentialAddress = fromOutputScript(result.data, network);
-  return { blindingKey: result.pubkey, unconfidentialAddress };
+  return {
+    blindingKey: result.pubkey,
+    unconfidentialAddress,
+    scriptPubKey: result.data,
+  };
 }
 
 function toConfidentialLegacy(
