aa67d72b3230e750a539494cede6ddbbd8ceb5cb
tiero <3596602+tiero@users.noreply.github.com>
2022-10-25 14:48:17 +0200

    re-order index.ts

--- ts_src/index.ts
+++ ts_src/index.ts
@@ -5,10 +5,6 @@ import * as payments from './payments';
 import * as script from './script';
 import * as issuance from './issuance';
 import * as bip341 from './bip341';
-export * from './asset';
-export * from './value';
-export * from './psetv2';
-export * from './confidential';
 
 export { address, crypto, networks, payments, script, issuance, bip341 };
 export { TaggedHashPrefix } from './crypto';
@@ -19,3 +15,7 @@ export {
   Transaction,
 } from './transaction';
 export { Network as NetworkExtended } from './networks';
+export * from './asset';
+export * from './value';
+export * from './psetv2';
+export * from './confidential';
